7b10c630063c376311b7792a5c7dce5e3715bf81
Ozi <oguzkaanyildiz@Ozis-MacBook-Air.local>
2022-06-09 20:44:11 +0200

    Making the Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+
 function App() {
   return (
     <div className="flex flex-col justify-between h-screen">
       <Navbar />
-
       <main>
         Content
       </main>
+      <Footer />
     </div>
   )
 }
