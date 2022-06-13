ebffff8b81729444f3c12763a33a43a935e77b64
Ozi <oguzkaanyildiz@Ozis-MacBook-Air.local>
2022-06-13 15:34:42 +0200

    Making the AlertContext and AlertReducer

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
 import { GithubProvider } from './components/context/github/GithubContext'
+import { AlertProvider } from './components/context/alert/AlertContext'
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
 
-          <main>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/notfound' element={<NotFound />} />
-            </Routes>
-          </main>
+            <main>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/notfound' element={<NotFound />} />
+              </Routes>
+            </main>
 
-          <Footer />
-        </div>
-      </Router>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   )
 }
